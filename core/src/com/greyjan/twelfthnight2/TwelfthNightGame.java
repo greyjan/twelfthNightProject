9647c87f0866e056411e2312f38cefe8c2c3738f
Jan Fic <janfc6@gmail.com>
2017-10-22 21:11:56 -0400

    added saveFile functionality

--- core/src/com/greyjan/twelfthnight2/TwelfthNightGame.java
+++ core/src/com/greyjan/twelfthnight2/TwelfthNightGame.java
@@ -3,23 +3,40 @@ package com.greyjan.twelfthnight2;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.greyjan.twelfthnight2.screens.LoadingScreen;
 import com.greyjan.twelfthnight2.screens.MainMenuScreen;
+import java.util.Scanner;
 
 public class TwelfthNightGame extends Game {
 
     MainMenuScreen mainMenu;
-    
+    FileHandle saveFile;
+    FileHandle[] saves;
+
     @Override
     public void create() {
+
+        saveFile = Gdx.files.external("twelfthNightGame/saves.txt");
+        saves = new FileHandle[3];
+        System.out.println(saveFile.exists());
+        System.out.println(saveFile.file().getAbsolutePath());
+        if (saveFile.exists()) {
+            readSaves();
+        } else {
+            saveFile.writeString("0", false);
+        }
+        
+        
+
         LoadingScreen loading = new LoadingScreen(this);
         loading.setILoadingListener(new LoadingScreen.ILoadingListener() {
 
             @Override
             public void OnFinished() {
                 mainMenu = new MainMenuScreen(TwelfthNightGame.this);
-                
+
                 Gdx.input.setInputProcessor(mainMenu.stage);
                 TwelfthNightGame.this.setScreen(mainMenu);
 
@@ -29,8 +46,22 @@ public class TwelfthNightGame extends Game {
         Gdx.input.setOnscreenKeyboardVisible(true);
     }
 
-   
-    
+    public void readSaves() {
+        String data = saveFile.readString();
+        Scanner scan = new Scanner(data);
+        int savesRecorded = Integer.parseInt(scan.next());
+        for(int i = 0; i < savesRecorded; i++) {
+            int coins = Integer.parseInt(scan.next());
+            int highScore = Integer.parseInt(scan.nextLine());
+            String name = scan.nextLine();
+            System.out.println("Loading saves");
+            System.out.println("Loading..." + name + "'s save");
+            System.out.println("Name: " + name);
+            System.out.println("Coins: " + coins);
+            System.out.println("Highscore: " + highScore);
+        }
+    }
+
     @Override
     public void render() {
         Color c = Color.SKY;
@@ -42,7 +73,5 @@ public class TwelfthNightGame extends Game {
         }
 
     }
-    
-    
 
 }
